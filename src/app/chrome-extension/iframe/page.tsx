3031164f38509054e97ebcb9baa41e284e4a7fd7
Syed-Perssonify <syed@perssonify.com>
2025-09-18 14:02:29 +0530

    Implement code changes to enhance functionality and improve performance

--- src/app/chrome-extension/iframe/page.tsx
+++ src/app/chrome-extension/iframe/page.tsx
@@ -1,17 +1,17 @@
-// import { Main } from "./_main";
+import { Main } from "./_main";
 
-// export default async function ChromeExtensionIframe({
-//   searchParams,
-// }: {
-//   searchParams: { [key: string]: string | string[] | undefined };
-// }) {
-//   console.log("Search Params:", searchParams);
-//   const token = (await searchParams).tkn || null;
-//   console.log("Token:", token);
-//   const data = (await searchParams).dt || null;
-//   const parsedData = data ? JSON.parse(decodeURIComponent(data as string)) : [];
-//   console.log("Active Cases:", parsedData?.activeCases || []);
-//   return (
-//     <Main activeCases={parsedData?.activeCases || []} token={token as string} />
-//   );
-// }
+export default async function ChromeExtensionIframe({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  console.log("Search Params:", searchParams);
+  const token = (await searchParams).tkn || null;
+  console.log("Token:", token);
+  const data = (await searchParams).dt || null;
+  const parsedData = data ? JSON.parse(decodeURIComponent(data as string)) : [];
+  console.log("Active Cases:", parsedData?.activeCases || []);
+  return (
+    <Main activeCases={parsedData?.activeCases || []} token={token as string} />
+  );
+}
