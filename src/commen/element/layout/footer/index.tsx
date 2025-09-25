54abe8c76ac234b893066051f5d33956e6aca982
Syed-Perssonify <syed@perssonify.com>
2025-09-25 17:30:07 +0530

    refactor: enhance type definitions for social and legal links in Footer component

--- src/commen/element/layout/footer/index.tsx
+++ src/commen/element/layout/footer/index.tsx
@@ -5,6 +5,14 @@ import { Linkedin, Mail, Phone, MapPin, Facebook } from "lucide-react";
 import { params } from "@/commen/config/params";
 import { routes } from "@/commen/config/routes";
 import { mediaUrl } from "@/commen/config/utils";
+import {
+  JSXElementConstructor,
+  Key,
+  ReactElement,
+  ReactNode,
+  ReactPortal,
+} from "react";
+import { UrlObject } from "url";
 
 function Footer() {
   const { footer } = params;
@@ -53,105 +61,143 @@ function Footer() {
             <div>
               <h3 className="font-semibold mb-4 text-primary">Connect</h3>
               <div className="flex items-center gap-4">
-                {footer.social.map((social, index) => (
-                  <Link
-                    key={index}
-                    href={social.href}
-                    className="text-black hover:text-primary"
-                  >
-                    {social.icon === "Facebook" && (
-                      <Facebook className="h-5 w-5" />
-                    )}
-                    {social.icon === "Linkedin" && (
-                      <Linkedin className="h-5 w-5" />
-                    )}
-                    {social.icon === "Twitter" && (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        className="bi bi-twitter-x"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-                      </svg>
-                    )}
-                    {social.icon === "Mail" && <Mail className="h-5 w-5" />}
-                  </Link>
-                ))}
+                {footer.social.map(
+                  (
+                    social: { href: string | UrlObject; icon: string },
+                    index: Key | null | undefined
+                  ) => (
+                    <Link
+                      key={index}
+                      href={social.href}
+                      className="text-black hover:text-primary"
+                    >
+                      {social.icon === "Facebook" && (
+                        <Facebook className="h-5 w-5" />
+                      )}
+                      {social.icon === "Linkedin" && (
+                        <Linkedin className="h-5 w-5" />
+                      )}
+                      {social.icon === "Twitter" && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          fill="currentColor"
+                          className="bi bi-twitter-x"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+                        </svg>
+                      )}
+                      {social.icon === "Mail" && <Mail className="h-5 w-5" />}
+                    </Link>
+                  )
+                )}
               </div>
             </div>
             <div>
               <h3 className="font-semibold mb-4 text-primary ">Legal</h3>
               <ul className="space-y-2">
-                {footer.legal.map((legal, index) => (
-                  <li key={index}>
-                    <Link
-                      href={legal.href}
-                      className="text-black hover:text-primary flex items-center gap-2"
-                    >
-                      {legal.label === "Privacy Policy" && (
-                        <svg
-                          width="30"
-                          height="25"
-                          viewBox="0 0 192 192"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="currentColor"
-                        >
-                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                          <g
-                            id="SVGRepo_tracerCarrier"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          ></g>
-                          <g id="SVGRepo_iconCarrier">
-                            {" "}
+                {footer.legal.map(
+                  (
+                    legal: {
+                      href: string | UrlObject;
+                      label:
+                        | string
+                        | number
+                        | bigint
+                        | boolean
+                        | ReactElement<
+                            unknown,
+                            string | JSXElementConstructor<any>
+                          >
+                        | Iterable<ReactNode>
+                        | Promise<
+                            | string
+                            | number
+                            | bigint
+                            | boolean
+                            | ReactPortal
+                            | ReactElement<
+                                unknown,
+                                string | JSXElementConstructor<any>
+                              >
+                            | Iterable<ReactNode>
+                            | null
+                            | undefined
+                          >
+                        | null
+                        | undefined;
+                    },
+                    index: Key | null | undefined
+                  ) => (
+                    <li key={index}>
+                      <Link
+                        href={legal.href}
+                        className="text-black hover:text-primary flex items-center gap-2"
+                      >
+                        {legal.label === "Privacy Policy" && (
+                          <svg
+                            width="30"
+                            height="25"
+                            viewBox="0 0 192 192"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                          >
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                             <g
-                              fill="none"
-                              stroke="currentColor"
-                              stroke-width="12"
-                            >
+                              id="SVGRepo_tracerCarrier"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></g>
+                            <g id="SVGRepo_iconCarrier">
                               {" "}
-                              <path
-                                stroke-linecap="round"
-                                d="M151.8 144.5a74 74 0 0 1-85.59 19.21A74 74 0 0 1 22.42 87.7a74 74 0 0 1 59.55-64.42m28.03.06a74 74 0 0 1 50.06 35.61 74 74 0 0 1 5.915 61.15"
-                              ></path>{" "}
-                              <path d="M76 92h40c4.432 0 8 3.568 8 8v22c0 4.432-3.568 8-8 8H76c-4.432 0-8-3.568-8-8v-22c0-4.432 3.568-8 8-8zm4 0V77.7C80 69.029 87.163 62 96 62s16 7.029 16 15.7V92"></path>{" "}
-                            </g>{" "}
-                          </g>
-                        </svg>
-                      )}
-                      {legal.label === "Terms of Service" && (
-                        <svg
-                          width="30"
-                          height="25"
-                          fill="currentColor"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 100 100"
-                          enable-background="new 0 0 100 100"
-                          xmlSpace="preserve"
-                        >
-                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                          <g
-                            id="SVGRepo_tracerCarrier"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          ></g>
-                          <g id="SVGRepo_iconCarrier">
-                            {" "}
-                            <g>
+                              <g
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="12"
+                              >
+                                {" "}
+                                <path
+                                  stroke-linecap="round"
+                                  d="M151.8 144.5a74 74 0 0 1-85.59 19.21A74 74 0 0 1 22.42 87.7a74 74 0 0 1 59.55-64.42m28.03.06a74 74 0 0 1 50.06 35.61 74 74 0 0 1 5.915 61.15"
+                                ></path>{" "}
+                                <path d="M76 92h40c4.432 0 8 3.568 8 8v22c0 4.432-3.568 8-8 8H76c-4.432 0-8-3.568-8-8v-22c0-4.432 3.568-8 8-8zm4 0V77.7C80 69.029 87.163 62 96 62s16 7.029 16 15.7V92"></path>{" "}
+                              </g>{" "}
+                            </g>
+                          </svg>
+                        )}
+                        {legal.label === "Terms of Service" && (
+                          <svg
+                            width="30"
+                            height="25"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 100 100"
+                            enable-background="new 0 0 100 100"
+                            xmlSpace="preserve"
+                          >
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g
+                              id="SVGRepo_tracerCarrier"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></g>
+                            <g id="SVGRepo_iconCarrier">
                               {" "}
-                              <path d="M69.1,28.9L55.9,15.7c-0.3-0.3-0.6-0.4-1-0.4c-0.8,0-1.4,0.6-1.4,1.4v10.6c0,2.2,1.8,4,4,4h10.6 c0.8,0,1.4-0.6,1.4-1.4C69.5,29.5,69.4,29.2,69.1,28.9z M69.5,39.3c0-1.1-0.9-2-2-2h-14c-3.3,0-6-2.7-6-6v-14c0-1.1-0.9-2-2-2h-20 c-3.3,0-6,2.7-6,6v48c0,3.3,2.7,6,6,6c0,0,18.7,0,20.3,0s2-1.1,1.9-2.8s-0.7-9,4.3-14.8s13.2-6.2,14.4-6.2c1.2,0,3.2-0.1,3.1-1.8 S69.5,39.3,69.5,39.3z M26.5,27.9l4.9-0.7c0.1,0,0.3-0.1,0.3-0.2l2.2-4.5c0.2-0.3,0.6-0.3,0.8,0l2.2,4.5c0.1,0.1,0.2,0.2,0.3,0.2 l4.9,0.7c0.3,0.1,0.5,0.5,0.2,0.7l-3.6,3.5c-0.1,0.1-0.1,0.2-0.1,0.4l0.8,4.9c0.1,0.3-0.3,0.6-0.6,0.4l-4.4-2.3 c-0.1-0.1-0.3-0.1-0.4,0l-4.4,2.3c-0.3,0.2-0.7-0.1-0.6-0.4l0.8-4.9c0-0.1,0-0.3-0.1-0.4l-3.6-3.5C26,28.4,26.2,28,26.5,27.9z M44.5,61.3c0,1.1-0.9,2-2,2h-13c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h13c1.1,0,2,0.9,2,2V61.3z M55.5,49.3c0,1.1-0.9,2-2,2h-24 c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h24c1.1,0,2,0.9,2,2V49.3z"></path>{" "}
-                              <path d="M66.3,56.2c-7.9,0-14.3,6.4-14.3,14.3s6.4,14.3,14.3,14.3s14.3-6.4,14.3-14.3S74.1,56.2,66.3,56.2z M68.8,71.8c-0.4,0-0.8-0.1-1.2-0.2L62.1,77c-0.4,0.4-0.8,0.5-1.2,0.5c-0.5,0-0.8-0.1-1.2-0.5c-0.6-0.6-0.6-1.7,0-2.4l5.5-5.5 C65.1,68.8,65,68.4,65,68c-0.2-2.5,1.8-4.8,4.3-4.8c0.4,0,0.8,0.1,1.2,0.2c0.2,0,0.2,0.2,0.1,0.4l-2.4,2.5c-0.2,0.1-0.2,0.5,0,0.6 l1.7,1.7c0.2,0.2,0.5,0.2,0.7,0l2.4-2.4c0.1-0.1,0.4-0.1,0.4,0.1c0.1,0.4,0.2,0.8,0.2,1.2C73.5,70,71.4,72,68.8,71.8z"></path>{" "}
-                            </g>{" "}
-                          </g>
-                        </svg>
-                      )}
-                      {legal.label}
-                    </Link>
-                  </li>
-                ))}
+                              <g>
+                                {" "}
+                                <path d="M69.1,28.9L55.9,15.7c-0.3-0.3-0.6-0.4-1-0.4c-0.8,0-1.4,0.6-1.4,1.4v10.6c0,2.2,1.8,4,4,4h10.6 c0.8,0,1.4-0.6,1.4-1.4C69.5,29.5,69.4,29.2,69.1,28.9z M69.5,39.3c0-1.1-0.9-2-2-2h-14c-3.3,0-6-2.7-6-6v-14c0-1.1-0.9-2-2-2h-20 c-3.3,0-6,2.7-6,6v48c0,3.3,2.7,6,6,6c0,0,18.7,0,20.3,0s2-1.1,1.9-2.8s-0.7-9,4.3-14.8s13.2-6.2,14.4-6.2c1.2,0,3.2-0.1,3.1-1.8 S69.5,39.3,69.5,39.3z M26.5,27.9l4.9-0.7c0.1,0,0.3-0.1,0.3-0.2l2.2-4.5c0.2-0.3,0.6-0.3,0.8,0l2.2,4.5c0.1,0.1,0.2,0.2,0.3,0.2 l4.9,0.7c0.3,0.1,0.5,0.5,0.2,0.7l-3.6,3.5c-0.1,0.1-0.1,0.2-0.1,0.4l0.8,4.9c0.1,0.3-0.3,0.6-0.6,0.4l-4.4-2.3 c-0.1-0.1-0.3-0.1-0.4,0l-4.4,2.3c-0.3,0.2-0.7-0.1-0.6-0.4l0.8-4.9c0-0.1,0-0.3-0.1-0.4l-3.6-3.5C26,28.4,26.2,28,26.5,27.9z M44.5,61.3c0,1.1-0.9,2-2,2h-13c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h13c1.1,0,2,0.9,2,2V61.3z M55.5,49.3c0,1.1-0.9,2-2,2h-24 c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h24c1.1,0,2,0.9,2,2V49.3z"></path>{" "}
+                                <path d="M66.3,56.2c-7.9,0-14.3,6.4-14.3,14.3s6.4,14.3,14.3,14.3s14.3-6.4,14.3-14.3S74.1,56.2,66.3,56.2z M68.8,71.8c-0.4,0-0.8-0.1-1.2-0.2L62.1,77c-0.4,0.4-0.8,0.5-1.2,0.5c-0.5,0-0.8-0.1-1.2-0.5c-0.6-0.6-0.6-1.7,0-2.4l5.5-5.5 C65.1,68.8,65,68.4,65,68c-0.2-2.5,1.8-4.8,4.3-4.8c0.4,0,0.8,0.1,1.2,0.2c0.2,0,0.2,0.2,0.1,0.4l-2.4,2.5c-0.2,0.1-0.2,0.5,0,0.6 l1.7,1.7c0.2,0.2,0.5,0.2,0.7,0l2.4-2.4c0.1-0.1,0.4-0.1,0.4,0.1c0.1,0.4,0.2,0.8,0.2,1.2C73.5,70,71.4,72,68.8,71.8z"></path>{" "}
+                              </g>{" "}
+                            </g>
+                          </svg>
+                        )}
+                        {legal.label}
+                      </Link>
+                    </li>
+                  )
+                )}
               </ul>
             </div>
           </div>
