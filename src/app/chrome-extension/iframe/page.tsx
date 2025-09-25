da00513028fcc8c4a92dbc0673d4fead368514fb
Syed-Perssonify <syed@perssonify.com>
2025-09-25 17:25:28 +0530

    refactor: update ChromeExtensionIframe to handle searchParams as a promise

--- src/app/chrome-extension/iframe/page.tsx
+++ src/app/chrome-extension/iframe/page.tsx
@@ -1,20 +1,19 @@
 import { Main } from "./_main";
 
-export default function ChromeExtensionIframe({
+export default async function ChromeExtensionIframe({
   searchParams,
 }: {
-  searchParams?: { [key: string]: string | string[] | undefined };
+  searchParams:
+    | Promise<{ [key: string]: string | string[] | undefined }>
+    | undefined;
 }) {
-  console.log("Search Params:", searchParams);
-
-  const token = searchParams?.tkn || null;
+  const resolvedSearchParams = searchParams ? await searchParams : {};
+  console.log("Search Params:", resolvedSearchParams);
+  const token = resolvedSearchParams.tkn || null;
   console.log("Token:", token);
-
-  const data = searchParams?.dt || null;
+  const data = resolvedSearchParams.dt || null;
   const parsedData = data ? JSON.parse(decodeURIComponent(data as string)) : [];
-
   console.log("Active Cases:", parsedData?.activeCases || []);
-
   return (
     <Main activeCases={parsedData?.activeCases || []} token={token as string} />
   );
