a8e15807cabb7d08b2fee6cb39ccb9418d2968fb
Syed-Perssonify <syed@perssonify.com>
2025-09-25 18:12:02 +0530

    refactor: remove commented-out baseUrl assignment logic in params configuration

--- src/commen/config/params.ts
+++ src/commen/config/params.ts
@@ -2,7 +2,4 @@ const params =
   process.env.NEXT_PUBLIC_PROJECT === "suewallstreet"
     ? require("./projects/suewallstreet").params
     : require("./projects/chromeextension").params;
-// process.env.NEXT_PUBLIC_MEDIA_URL === "suewallstreet"
-//   ? (params.baseUrl = "https://www.suewallstreet.com")
-//   : (params.baseUrl = "https://www.chromeextension.com");
 export { params };
