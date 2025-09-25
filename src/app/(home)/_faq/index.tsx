39a9bb3355451ddd0a9318b6e95942daf2c05df4
Syed-Perssonify <syed@perssonify.com>
2025-09-25 17:27:28 +0530

    refactor: enhance type definitions for FAQ items in Accordion component

--- src/app/(home)/_faq/index.tsx
+++ src/app/(home)/_faq/index.tsx
@@ -8,6 +8,13 @@ import {
 } from "@/components/ui/accordion";
 import Link from "next/link";
 import { params } from "@/commen/config/params";
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+} from "react";
 
 export default function FAQ() {
   const { faq } = params;
@@ -30,22 +37,74 @@ export default function FAQ() {
             collapsible
             className="bg-card ring-primary w-full rounded-2xl border px-4 sm:px-8 py-3 ring-4"
           >
-            {faq.items.map((item) => (
-              <AccordionItem
-                key={item.id}
-                value={item.id}
-                className="border-dashed"
-              >
-                <AccordionTrigger className="cursor-pointer hover:text-primary text-sm sm:text-base  font-galano-medium text-dark-green">
-                  {item.question}
-                </AccordionTrigger>
-                <AccordionContent>
-                  <p className="text-sm sm:text-base font-galano text-gray leading-relaxed">
-                    {item.answer}
-                  </p>
-                </AccordionContent>
-              </AccordionItem>
-            ))}
+            {faq.items.map(
+              (item: {
+                id: Key | null | undefined;
+                question:
+                  | string
+                  | number
+                  | bigint
+                  | boolean
+                  | ReactElement<unknown, string | JSXElementConstructor<any>>
+                  | Iterable<ReactNode>
+                  | ReactPortal
+                  | Promise<
+                      | string
+                      | number
+                      | bigint
+                      | boolean
+                      | ReactPortal
+                      | ReactElement<
+                          unknown,
+                          string | JSXElementConstructor<any>
+                        >
+                      | Iterable<ReactNode>
+                      | null
+                      | undefined
+                    >
+                  | null
+                  | undefined;
+                answer:
+                  | string
+                  | number
+                  | bigint
+                  | boolean
+                  | ReactElement<unknown, string | JSXElementConstructor<any>>
+                  | Iterable<ReactNode>
+                  | ReactPortal
+                  | Promise<
+                      | string
+                      | number
+                      | bigint
+                      | boolean
+                      | ReactPortal
+                      | ReactElement<
+                          unknown,
+                          string | JSXElementConstructor<any>
+                        >
+                      | Iterable<ReactNode>
+                      | null
+                      | undefined
+                    >
+                  | null
+                  | undefined;
+              }) => (
+                <AccordionItem
+                  key={item.id}
+                  value={item.id?.toString() ?? ""}
+                  className="border-dashed"
+                >
+                  <AccordionTrigger className="cursor-pointer hover:text-primary text-sm sm:text-base  font-galano-medium text-dark-green">
+                    {item.question}
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <p className="text-sm sm:text-base font-galano text-gray leading-relaxed">
+                      {item.answer}
+                    </p>
+                  </AccordionContent>
+                </AccordionItem>
+              )
+            )}
           </Accordion>
 
           <p className="text-muted-foreground text-center mt-6 sm:mt-8 font-galano text-gray">
