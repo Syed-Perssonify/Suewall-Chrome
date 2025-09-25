6b760869b57a717fefd36bae8a316fb433e45714
Syed-Perssonify <syed@perssonify.com>
2025-09-25 18:45:29 +0530

    refactor: simplify params assignment by removing environment condition

--- src/commen/config/params.ts
+++ src/commen/config/params.ts
@@ -1,5 +1,2 @@
-const params =
-  process.env.NEXT_PUBLIC_PROJECT === "suewallstreet"
-    ? require("./projects/suewallstreet").params
-    : require("./projects/chromeextension").params;
+const params = require("./projects/suewallstreet").params;
 export { params };
